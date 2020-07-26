396399800aca1dcac030f4fab4595f8150f26ffc
Simon Mourier <simon_mourier@hotmail.com>
2020-07-27 00:41:20 +0200

    tests

--- ExplorerPreview/ExplorerPreview.cpp
+++ ExplorerPreview/ExplorerPreview.cpp
@@ -1,180 +1,396 @@
-// ExplorerPreview.cpp : Defines the entry point for the application.
-//
-
 #include "pch.h"
 #include "framework.h"
 #include "ExplorerPreview.h"
 
+#define CHECKHR(__expr) { HRESULT hr = __expr; if (FAILED(hr)) { ATLTRACE(L"Failed hr:0x%08X", hr); return hr;} }
+
+CComPtr<IPreviewHandler> handler;
+
+class CTest : public IUnknown
+{
+	LONG m_ref;
+
+public:
+	CTest() : m_ref(1) { }
+
+	IFACEMETHODIMP QueryInterface(REFIID riid, void** ppv)
+	{
+		if (riid == IID_IUnknown)
+		{
+			*ppv = static_cast<IUnknown*>(this);
+			AddRef();
+			return S_OK;
+		}
+		*ppv = NULL;
+
+		CComHeapPtr<wchar_t> id;
+		StringFromCLSID(riid, &id);
+		ATLTRACE(L"QueryInterface iid: '%s'", id);
+		return E_NOINTERFACE;
+	}
+
+	IFACEMETHODIMP_(ULONG) AddRef() { return InterlockedIncrement(&m_ref); }
+	IFACEMETHODIMP_(ULONG) Release() { LONG reg = InterlockedDecrement(&m_ref); if (!reg) delete this; return reg; }
+};
+
+CTest test;
+
+template<typename DispInterface>
+class CDispInterfaceBase : public DispInterface
+{
+	LONG m_ref;
+	CComPtr<IConnectionPoint> m_point;
+	DWORD m_cookie;
+
+public:
+	CDispInterfaceBase() : m_ref(1), m_cookie(0) { }
+
+	IFACEMETHODIMP QueryInterface(REFIID riid, void** ppv)
+	{
+		if (riid == IID_IUnknown || riid == IID_IDispatch || riid == __uuidof(DispInterface))
+		{
+			*ppv = static_cast<DispInterface*>(static_cast<IDispatch*>(this));
+			AddRef();
+			return S_OK;
+		}
+		*ppv = NULL;
+		return E_NOINTERFACE;
+	}
+
+	IFACEMETHODIMP_(ULONG) AddRef() { return InterlockedIncrement(&m_ref); }
+	IFACEMETHODIMP_(ULONG) Release() { LONG reg = InterlockedDecrement(&m_ref); if (!reg) delete this; return reg; }
+
+	IFACEMETHODIMP GetTypeInfoCount(UINT* pctinfo) { *pctinfo = 0; return E_NOTIMPL; }
+	IFACEMETHODIMP GetTypeInfo(UINT iTInfo, LCID lcid, ITypeInfo** ppTInfo) { *ppTInfo = nullptr; return E_NOTIMPL; }
+	IFACEMETHODIMP GetIDsOfNames(REFIID, LPOLESTR* rgszNames, UINT cNames, LCID lcid, DISPID* rgDispId) { return E_NOTIMPL; }
+	IFACEMETHODIMP Invoke(DISPID dispid, REFIID riid, LCID lcid, WORD wFlags, DISPPARAMS* pdispparams, VARIANT* pvarResult, EXCEPINFO* pexcepinfo, UINT* puArgErr)
+	{
+		if (pvarResult)
+		{
+			VariantInit(pvarResult);
+		}
+		return Invoke(dispid, pdispparams, pvarResult);
+	}
+
+	virtual HRESULT Invoke(DISPID dispid, DISPPARAMS* pdispparams, VARIANT* pvarResult) = 0;
+
+	HRESULT Connect(IUnknown* punk)
+	{
+		CComPtr<IConnectionPointContainer> container;
+		CHECKHR(punk->QueryInterface(IID_PPV_ARGS(&container)));
+		CHECKHR(container->FindConnectionPoint(__uuidof(DispInterface), &m_point));
+		return m_point->Advise(this, &m_cookie);
+	}
+
+	void Disconnect()
+	{
+		if (m_cookie)
+		{
+			m_point->Unadvise(m_cookie);
+			m_point.Release();
+			m_cookie = 0;
+		}
+	}
+};
+
+class WebBrowserEvents : public CDispInterfaceBase<DWebBrowserEvents2>
+{
+	HRESULT Invoke(DISPID dispid, DISPPARAMS* pDispParams, VARIANT* pvarResult)
+	{
+		// there are other ways to find selection changed, but this one is simple since it only uses webbrowser (explorer) events
+		if (dispid == DISPID_PROGRESSCHANGE)
+		{
+			// when progress = progress end, it's usable
+			if (V_I4(&pDispParams->rgvarg[0]) == V_I4(&pDispParams->rgvarg[1]))
+			{
+				if (handler)
+				{
+					CComPtr<IObjectWithSite> site;
+					if (SUCCEEDED(handler->QueryInterface(&site)))
+					{
+						site->SetSite(NULL);
+					}
+
+					handler->Unload();
+					handler.Release();
+				}
+
+				CComPtr<IShellBrowser> browser;
+				CHECKHR(CComQIPtr<IServiceProvider>(m_window)->QueryService(SID_STopLevelBrowser, IID_PPV_ARGS(&browser)));
+
+				CComPtr<IShellView> view;
+				CHECKHR(browser->QueryActiveShellView(&view));
+
+				CComPtr<IFolderView2> folderView;
+				CHECKHR(view->QueryInterface(&folderView));
+
+				// get selected item
+				int index = -1;
+				folderView->GetSelectedItem(0, &index);
+				if (index >= 0)
+				{
+					CComPtr<IShellItem2> item;
+					CHECKHR(folderView->GetItem(index, IID_PPV_ARGS(&item)));
+
+					CComHeapPtr<wchar_t> ext;
+					CHECKHR(item->GetString(PKEY_ItemType, &ext));
+
+					CComHeapPtr<wchar_t> parsingName;
+					CHECKHR(item->GetDisplayName(SIGDN::SIGDN_DESKTOPABSOLUTEPARSING, &parsingName));
+
+					ATLTRACE(L"WebBrowserEvent selected: '%s' ext: '%s'", parsingName, ext);
+
+					CComPtr<IQueryAssociations> assoc;
+					CHECKHR(item->BindToHandler(NULL, BHID_AssociationArray, IID_PPV_ARGS(&assoc)));
+
+					WCHAR sclsid[48] = { 0 };
+					DWORD size = 48;
+					CHECKHR(assoc->GetString(ASSOCF_INIT_DEFAULTTOSTAR, ASSOCSTR_SHELLEXTENSION, L"{8895b1c6-b41f-4c1c-a562-0d564250836f}", sclsid, &size));
+					ATLTRACE(L"WebBrowserEvent clsid: '%s'", sclsid);
+
+					CLSID clsid;
+					SHCLSIDFromString(sclsid, &clsid);
+					CHECKHR(handler.CoCreateInstance(clsid, NULL, CLSCTX_LOCAL_SERVER));
+
+					CComPtr<IObjectWithSite> site;
+					if (SUCCEEDED(handler->QueryInterface(&site)))
+					{
+						site->SetSite(&test);
+					}
+
+					CComPtr<IInitializeWithItem> iitem;
+					if (SUCCEEDED(handler->QueryInterface(&iitem)))
+					{
+						CHECKHR(iitem->Initialize(item, STGM_READ));
+					}
+					else
+					{
+						CComPtr<IInitializeWithFile> ifile;
+						if (SUCCEEDED(handler->QueryInterface(&ifile)))
+						{
+							CHECKHR(ifile->Initialize(parsingName, STGM_READ));
+						}
+						else
+						{
+							CComPtr<IInitializeWithStream> istream;
+							CHECKHR(handler->QueryInterface(&istream));
+
+							CComPtr<IStream> stream;
+							CHECKHR(SHCreateStreamOnFile(parsingName, STGM_READ, &stream));
+							CHECKHR(istream->Initialize(stream, STGM_READ));
+						}
+					}
+
+					RECT rc;
+					GetClientRect(m_hwnd, &rc);
+					CHECKHR(handler->SetWindow(m_hwnd, &rc));
+					CHECKHR(handler->DoPreview());
+					HWND hwnd;
+					handler->QueryFocus(&hwnd);
+				}
+			}
+		}
+		return S_OK;
+	}
+
+public:
+	CComPtr<IDispatch> m_window;
+	HWND m_hwnd;
+};
+
+class ShellWindowsEvents : public CDispInterfaceBase<DShellWindowsEvents>
+{
+	CAtlArray<WebBrowserEvents*> m_views;
+	HWND m_hwnd;
+
+	HRESULT HookViews()
+	{
+		Stop();
+		CComPtr<IShellWindows> windows;
+		CHECKHR(windows.CoCreateInstance(CLSID_ShellWindows));
+		long count = 0;
+		windows->get_Count(&count);
+		for (long i = 0; i < count; i++)
+		{
+			CComPtr<IDispatch> window;
+			CHECKHR(windows->Item(CComVariant(i), &window));
+
+			CComPtr<IWebBrowserApp> ebrowser;
+			CHECKHR(CComQIPtr<IServiceProvider>(window)->QueryService(SID_SWebBrowserApp, IID_PPV_ARGS(&ebrowser)));
+
+			WebBrowserEvents* events = new WebBrowserEvents();
+			events->m_hwnd = m_hwnd;
+			window.QueryInterface(&events->m_window);
+			if (FAILED(events->Connect(ebrowser)))
+			{
+				delete events;
+			}
+			else
+			{
+				m_views.Add(events);
+			}
+		}
+		return S_OK;
+	}
+
+	HRESULT Invoke(DISPID dispid, DISPPARAMS* pDispParams, VARIANT* pvarResult)
+	{
+		// unfortunately, FindWindowSW doesn't seem to work with a cookie (!?)
+		// so we reset the whole collection each time...
+		HookViews();
+		return S_OK;
+	}
+
+public:
+
+	HRESULT Start(HWND hwnd)
+	{
+		m_hwnd = hwnd;
+		CComPtr<IShellWindows> windows;
+		CHECKHR(windows.CoCreateInstance(CLSID_ShellWindows));
+		return Connect(windows);
+	}
+
+	void Stop()
+	{
+		for (size_t i = 0; i < m_views.GetCount(); i++)
+		{
+			WebBrowserEvents* p = m_views.GetAt(i);
+			p->Disconnect();
+			delete p;
+		}
+		m_views.RemoveAll();
+	}
+};
+
 #define MAX_LOADSTRING 100
 
-// Global Variables:
-HINSTANCE hInst;                                // current instance
-WCHAR szTitle[MAX_LOADSTRING];                  // The title bar text
-WCHAR szWindowClass[MAX_LOADSTRING];            // the main window class name
+ShellWindowsEvents windowEvents;
+HINSTANCE hInst;
+WCHAR szTitle[MAX_LOADSTRING];
+WCHAR szWindowClass[MAX_LOADSTRING];
 
-// Forward declarations of functions included in this code module:
-ATOM                MyRegisterClass(HINSTANCE hInstance);
-BOOL                InitInstance(HINSTANCE, int);
-LRESULT CALLBACK    WndProc(HWND, UINT, WPARAM, LPARAM);
-INT_PTR CALLBACK    About(HWND, UINT, WPARAM, LPARAM);
+ATOM MyRegisterClass(HINSTANCE hInstance);
+HWND InitInstance(HINSTANCE, int);
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+INT_PTR CALLBACK About(HWND, UINT, WPARAM, LPARAM);
 
-int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
-                     _In_opt_ HINSTANCE hPrevInstance,
-                     _In_ LPWSTR    lpCmdLine,
-                     _In_ int       nCmdShow)
+int APIENTRY wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nCmdShow)
 {
-    UNREFERENCED_PARAMETER(hPrevInstance);
-    UNREFERENCED_PARAMETER(lpCmdLine);
+	UNREFERENCED_PARAMETER(hPrevInstance);
+	UNREFERENCED_PARAMETER(lpCmdLine);
 
-    // TODO: Place code here.
+	CoInitialize(NULL);
+	LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
+	LoadStringW(hInstance, IDC_EXPLORERPREVIEW, szWindowClass, MAX_LOADSTRING);
+	MyRegisterClass(hInstance);
 
-    // Initialize global strings
-    LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
-    LoadStringW(hInstance, IDC_EXPLORERPREVIEW, szWindowClass, MAX_LOADSTRING);
-    MyRegisterClass(hInstance);
+	HWND hwnd = InitInstance(hInstance, nCmdShow);
+	if (!hwnd)
+		return FALSE;
 
-    // Perform application initialization:
-    if (!InitInstance (hInstance, nCmdShow))
-    {
-        return FALSE;
-    }
-
-    HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_EXPLORERPREVIEW));
-
-    MSG msg;
-
-    // Main message loop:
-    while (GetMessage(&msg, nullptr, 0, 0))
-    {
-        if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
-        {
-            TranslateMessage(&msg);
-            DispatchMessage(&msg);
-        }
-    }
-
-    return (int) msg.wParam;
+	windowEvents.Start(hwnd);
+	HACCEL hAccelTable = LoadAccelerators(hInstance, MAKEINTRESOURCE(IDC_EXPLORERPREVIEW));
+	MSG msg;
+	while (GetMessage(&msg, nullptr, 0, 0))
+	{
+		if (!TranslateAccelerator(msg.hwnd, hAccelTable, &msg))
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+	windowEvents.Stop();
+	CoUninitialize();
+	return (int)msg.wParam;
 }
 
-
-
-//
-//  FUNCTION: MyRegisterClass()
-//
-//  PURPOSE: Registers the window class.
-//
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-    WNDCLASSEXW wcex;
-
-    wcex.cbSize = sizeof(WNDCLASSEX);
-
-    wcex.style          = CS_HREDRAW | CS_VREDRAW;
-    wcex.lpfnWndProc    = WndProc;
-    wcex.cbClsExtra     = 0;
-    wcex.cbWndExtra     = 0;
-    wcex.hInstance      = hInstance;
-    wcex.hIcon          = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_EXPLORERPREVIEW));
-    wcex.hCursor        = LoadCursor(nullptr, IDC_ARROW);
-    wcex.hbrBackground  = (HBRUSH)(COLOR_WINDOW+1);
-    wcex.lpszMenuName   = MAKEINTRESOURCEW(IDC_EXPLORERPREVIEW);
-    wcex.lpszClassName  = szWindowClass;
-    wcex.hIconSm        = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
-
-    return RegisterClassExW(&wcex);
+	WNDCLASSEXW wcex;
+	wcex.cbSize = sizeof(WNDCLASSEX);
+	wcex.style = CS_HREDRAW | CS_VREDRAW;
+	wcex.lpfnWndProc = WndProc;
+	wcex.cbClsExtra = 0;
+	wcex.cbWndExtra = 0;
+	wcex.hInstance = hInstance;
+	wcex.hIcon = LoadIcon(hInstance, MAKEINTRESOURCE(IDI_EXPLORERPREVIEW));
+	wcex.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	wcex.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+	wcex.lpszMenuName = MAKEINTRESOURCEW(IDC_EXPLORERPREVIEW);
+	wcex.lpszClassName = szWindowClass;
+	wcex.hIconSm = LoadIcon(wcex.hInstance, MAKEINTRESOURCE(IDI_SMALL));
+	return RegisterClassExW(&wcex);
 }
 
-//
-//   FUNCTION: InitInstance(HINSTANCE, int)
-//
-//   PURPOSE: Saves instance handle and creates main window
-//
-//   COMMENTS:
-//
-//        In this function, we save the instance handle in a global variable and
-//        create and display the main program window.
-//
-BOOL InitInstance(HINSTANCE hInstance, int nCmdShow)
+HWND InitInstance(HINSTANCE hInstance, int nCmdShow)
 {
-   hInst = hInstance; // Store instance handle in our global variable
+	hInst = hInstance;
+	HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, 10, 10, 800, 800, nullptr, nullptr, hInstance, nullptr);
+	if (!hWnd)
+		return NULL;
 
-   HWND hWnd = CreateWindowW(szWindowClass, szTitle, WS_OVERLAPPEDWINDOW, 10, 10, 800, 800, nullptr, nullptr, hInstance, nullptr);
-
-   if (!hWnd)
-   {
-      return FALSE;
-   }
-
-   ShowWindow(hWnd, nCmdShow);
-   UpdateWindow(hWnd);
-
-   return TRUE;
+	ShowWindow(hWnd, nCmdShow);
+	UpdateWindow(hWnd);
+	return hWnd;
 }
 
-//
-//  FUNCTION: WndProc(HWND, UINT, WPARAM, LPARAM)
-//
-//  PURPOSE: Processes messages for the main window.
-//
-//  WM_COMMAND  - process the application menu
-//  WM_PAINT    - Paint the main window
-//  WM_DESTROY  - post a quit message and return
-//
-//
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    switch (message)
-    {
-    case WM_COMMAND:
-        {
-            int wmId = LOWORD(wParam);
-            // Parse the menu selections:
-            switch (wmId)
-            {
-            case IDM_ABOUT:
-                DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
-                break;
-            case IDM_EXIT:
-                DestroyWindow(hWnd);
-                break;
-            default:
-                return DefWindowProc(hWnd, message, wParam, lParam);
-            }
-        }
-        break;
-    case WM_PAINT:
-        {
-            PAINTSTRUCT ps;
-            HDC hdc = BeginPaint(hWnd, &ps);
-            // TODO: Add any drawing code that uses hdc here...
-            EndPaint(hWnd, &ps);
-        }
-        break;
-    case WM_DESTROY:
-        PostQuitMessage(0);
-        break;
-    default:
-        return DefWindowProc(hWnd, message, wParam, lParam);
-    }
-    return 0;
+	switch (message)
+	{
+	case WM_COMMAND:
+	{
+		int wmId = LOWORD(wParam);
+		switch (wmId)
+		{
+		case IDM_ABOUT:
+			DialogBox(hInst, MAKEINTRESOURCE(IDD_ABOUTBOX), hWnd, About);
+			break;
+
+		case IDM_EXIT:
+			DestroyWindow(hWnd);
+			break;
+
+		default:
+			return DefWindowProc(hWnd, message, wParam, lParam);
+		}
+	}
+	break;
+
+	case WM_PAINT:
+	{
+		PAINTSTRUCT ps;
+		HDC hdc = BeginPaint(hWnd, &ps);
+		EndPaint(hWnd, &ps);
+	}
+	break;
+
+	case WM_DESTROY:
+		PostQuitMessage(0);
+		break;
+
+	default:
+		return DefWindowProc(hWnd, message, wParam, lParam);
+	}
+	return 0;
 }
 
-// Message handler for about box.
 INT_PTR CALLBACK About(HWND hDlg, UINT message, WPARAM wParam, LPARAM lParam)
 {
-    UNREFERENCED_PARAMETER(lParam);
-    switch (message)
-    {
-    case WM_INITDIALOG:
-        return (INT_PTR)TRUE;
+	UNREFERENCED_PARAMETER(lParam);
+	switch (message)
+	{
+	case WM_INITDIALOG:
+		return (INT_PTR)TRUE;
 
-    case WM_COMMAND:
-        if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
-        {
-            EndDialog(hDlg, LOWORD(wParam));
-            return (INT_PTR)TRUE;
-        }
-        break;
-    }
-    return (INT_PTR)FALSE;
+	case WM_COMMAND:
+		if (LOWORD(wParam) == IDOK || LOWORD(wParam) == IDCANCEL)
+		{
+			EndDialog(hDlg, LOWORD(wParam));
+			return (INT_PTR)TRUE;
+		}
+		break;
+	}
+	return (INT_PTR)FALSE;
 }
--- ExplorerPreview/framework.h
+++ ExplorerPreview/framework.h
@@ -1,10 +1,17 @@
 #pragma once
 
 #include "targetver.h"
-#define WIN32_LEAN_AND_MEAN             // Exclude rarely-used stuff from Windows headers
-// Windows Header Files
+#define WIN32_LEAN_AND_MEAN
 #include <windows.h>
-// C RunTime Header Files
+#include <atlbase.h>
+#include <atlcom.h>
+#include <atlcoll.h>
+#include <shobjidl_core.h>
+#include <shlobj_core.h>
+#include <exdispid.h>
+#include <shdispid.h>
+#include <propsys.h>
+#include <propkey.h>
 #include <stdlib.h>
 #include <malloc.h>
 #include <memory.h>
